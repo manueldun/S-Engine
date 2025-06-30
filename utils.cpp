9c4792e857fcb0825153dd8b4a1b94f79c37b2a4
Manuel Dun <manueldun@gmail.com>
2025-06-30 08:36:13 -0400

    Added some utility functions

--- utils.cpp
+++ utils.cpp
@@ -1,5 +1,11 @@
 #include "utils.h"
+#include "glm/fwd.hpp"
+#include "glm/geometric.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include <cassert>
 #include <fstream>
+#include <iostream>
+#include <span>
 std::vector<char> readFile(const std::string &filename) {
   std::ifstream file(filename, std::ios::ate | std::ios::binary);
 
@@ -15,4 +21,333 @@ std::vector<char> readFile(const std::string &filename) {
   file.close();
 
   return buffer;
+}
+/* Determines if the normal of a triangle points towards a point following the
+ * right hand ordering to determinate the triangle normal direction
+ */
+constexpr bool isTowardsPlaneNormal(std::array<glm::vec3, 3> trianglePlane,
+                                    glm::vec3 point) {
+
+  glm::vec3 triangleEdge1 = trianglePlane[1] - trianglePlane[0];
+  glm::vec3 triangleEdge2 = trianglePlane[2] - trianglePlane[0];
+  glm::vec3 normal = glm::cross(triangleEdge1, triangleEdge2);
+  float dotProduct = glm::dot(normal, point);
+  return dotProduct > 0.0f;
+}
+
+tinygltf::Model loadGltfFile(const std::string &path) {
+  tinygltf::Model model;
+  tinygltf::TinyGLTF loader;
+  std::string err;
+  std::string warn;
+
+  bool ret = loader.LoadASCIIFromFile(&model, &err, &warn, path);
+
+  if (!warn.empty()) {
+    std::cout << "Warn: " << warn << std::endl;
+  }
+
+  if (!ret) {
+    throw std::runtime_error("Error loading gltf file");
+  }
+
+  if (!err.empty()) {
+    std::cout << "Error: " << err << std::endl;
+  }
+  return model;
+}
+std::vector<std::span<glm::vec3>>
+getVerticeData(const tinygltf::Model &model, const std::string &attributeName) {
+  std::vector<std::span<glm::vec3>> bufferSpans;
+
+  for (const tinygltf::Node &node : model.nodes) {
+    tinygltf::Mesh mesh = model.meshes[node.mesh];
+    assert(mesh.primitives.size() > 1);
+    assert(mesh.primitives[0].attributes.count(attributeName));
+    if (mesh.primitives.size() > 0) {
+
+      const int primitiveIndex = mesh.primitives[0].attributes[attributeName];
+      tinygltf::Accessor accessor = model.accessors[primitiveIndex];
+      const int bufferViewIndex = accessor.bufferView;
+      tinygltf::BufferView bufferView = model.bufferViews[bufferViewIndex];
+      const int bufferIndex = bufferView.buffer;
+      tinygltf::Buffer buffer = model.buffers[bufferIndex];
+      assert(accessor.componentType == TINYGLTF_COMPONENT_TYPE_FLOAT);
+      const size_t componentBytes{4};
+      size_t numberOfComponents{0};
+      switch (accessor.type) {
+      case TINYGLTF_TYPE_SCALAR:
+        numberOfComponents = 1;
+        break;
+      case TINYGLTF_TYPE_VEC2:
+        numberOfComponents = 2;
+        break;
+      case TINYGLTF_TYPE_VEC3:
+        numberOfComponents = 3;
+        break;
+      case TINYGLTF_TYPE_VEC4:
+        numberOfComponents = 4;
+        break;
+      default:
+        std::cerr << "accessor type not supported" << std::endl;
+        assert(false);
+      }
+      const size_t numberOfVertices =
+          bufferView.byteLength / componentBytes / numberOfComponents;
+      glm::vec3 *vec3ptr = reinterpret_cast<glm::vec3 *>(buffer.data.data());
+      std::span<glm::vec3> vecBufferSpan(vec3ptr, numberOfVertices);
+      bufferSpans.push_back(vecBufferSpan);
+    }
+  }
+
+  return bufferSpans;
+}
+template <typename T> std::span<T> IndexDataSpan::getIndexSpan() {
+
+  switch (m_bytesPerInt) {
+  case 1: {
+    uint8_t *intptr{reinterpret_cast<uint8_t *>(m_indexSpan.data())};
+    size_t indexLength = m_indexSpan.size();
+    return std::span<uint8_t>(intptr, intptr + indexLength);
+    break;
+  }
+  case 2: {
+    uint16_t *intptr{reinterpret_cast<uint16_t *>(m_indexSpan.data())};
+    size_t indexLength = m_indexSpan.size() / 2;
+    return std::span<uint16_t>(intptr, intptr + indexLength);
+    break;
+  }
+  case 4: {
+    uint32_t *intptr{reinterpret_cast<uint32_t *>(m_indexSpan.data())};
+    size_t indexLength = m_indexSpan.size();
+    return std::span<uint32_t>(intptr, intptr + indexLength);
+    break;
+  }
+  }
+}
+template <typename T>
+std::vector<std::span<T>> getIndexData(const tinygltf::Model &model) {
+
+  std::vector<std::span<T>> bufferSpans;
+
+  for (const tinygltf::Node &node : model.nodes) {
+    tinygltf::Mesh mesh = model.meshes[node.mesh];
+    assert(mesh.primitives.size() > 1);
+    if (mesh.primitives.size() > 0) {
+
+      const int accesorIndex{mesh.primitives[0].indices};
+      tinygltf::Accessor accessor = model.accessors[accesorIndex];
+      const int bufferViewIndex{accessor.bufferView};
+      tinygltf::BufferView bufferView = model.bufferViews[bufferViewIndex];
+      const int bufferIndex = bufferView.buffer;
+      tinygltf::Buffer buffer{model.buffers[bufferIndex]};
+      size_t componentBytes;
+      switch (accessor.componentType) {
+      case TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE: {
+        componentBytes = 1;
+        const size_t numberOfIndices = bufferView.byteLength / componentBytes;
+        uint8_t *intPtr = reinterpret_cast<uint8_t *>(buffer.data.data());
+        std::span<uint8_t> bufferSpan(intPtr, numberOfIndices);
+        bufferSpans.push_back(bufferSpan);
+        break;
+      }
+      case TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT: {
+        componentBytes = 2;
+        const size_t numberOfIndices = bufferView.byteLength / componentBytes;
+        uint16_t *intPtr = reinterpret_cast<uint16_t *>(buffer.data.data());
+        std::span<uint16_t> bufferSpan(intPtr, numberOfIndices);
+        bufferSpans.push_back(bufferSpan);
+        break;
+      }
+      case TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT: {
+        componentBytes = 4;
+        const size_t numberOfIndices = bufferView.byteLength / componentBytes;
+        uint32_t *intPtr = reinterpret_cast<uint32_t *>(buffer.data.data());
+        std::span<uint32_t> bufferSpan(intPtr, numberOfIndices);
+        bufferSpans.push_back(bufferSpan);
+        break;
+      }
+      }
+      assert(accessor.type == TINYGLTF_TYPE_SCALAR);
+    }
+  }
+
+  return bufferSpans;
+}
+template <typename T>
+constexpr glm::vec3 getCenterOfMass(const std::span<glm::vec3> &vertices,
+                                    const std::span<T> &indices,
+                                    const bool &verbose) {
+
+  unsigned int numOfVertices{0};
+  glm::vec3 centerOfMass{0.0f};
+  float totalArea{0.0f};
+
+  if (verbose) {
+    std::cout << "numOfTriangles: " << numOfVertices << std::endl;
+  }
+  for (size_t vertexIndex = 0; vertexIndex < numOfVertices; vertexIndex += 3) {
+    if (verbose) {
+      std::cout << "vertexIndex: " << vertexIndex << std::endl;
+      std::cout << "indices" << std::endl;
+      std::cout << indices[vertexIndex] << std::endl;
+      std::cout << indices[vertexIndex + 1] << std::endl;
+      std::cout << indices[vertexIndex + 2] << std::endl;
+    }
+
+    glm::vec3 vertex1{vertices[indices[vertexIndex]]};
+    if (verbose) {
+      std::cout << "vec1" << std::endl;
+
+      std::cout << vertex1.x << " " << vertex1.y << " " << vertex1.z
+                << std::endl;
+    }
+    glm::vec3 vertex2{vertices[indices[vertexIndex + 1]]};
+    if (verbose) {
+      std::cout << "vec2" << std::endl;
+      std::cout << vertex2.x << " " << vertex2.y << " " << vertex2.z
+                << std::endl;
+    }
+    glm::vec3 vertex3{vertices[indices[vertexIndex + 2]]};
+    if (verbose) {
+      std::cout << "vec3" << std::endl;
+      std::cout << vertex3.x << " " << vertex3.y << " " << vertex3.z
+                << std::endl;
+    }
+
+    glm::vec3 triangleCentroid = (vertex1 + vertex2 + vertex3) / 3.0f;
+
+    if (verbose) {
+      std::cout << "centroid" << std::endl;
+      std::cout << triangleCentroid.x << " " << triangleCentroid.y << " "
+                << triangleCentroid.z << std::endl;
+    }
+    glm::vec3 a{vertex2 - vertex1};
+    glm::vec3 b{vertex3 - vertex1};
+    glm::vec3 crossVector{glm::cross(a, b)};
+    float area{crossVector.length() / 2.0f};
+    totalArea += area;
+    centerOfMass += triangleCentroid * area;
+    if (verbose) {
+      std::cout << "Acumulated center of mass:" << std::endl;
+      std::cout << centerOfMass.x << " " << centerOfMass.y << " "
+                << centerOfMass.z << std::endl;
+    }
+  }
+  return centerOfMass / totalArea;
+}
+template <typename T>
+constexpr glm::mat4 getInertiaTensor(const std::span<glm::vec3> &vertices,
+                                     const std::span<T> &indices,
+                                     const bool &verbose) {
+
+  glm::mat3 acumulatedInertialTensor = glm::mat3(0.0f);
+
+  uint32_t numsOfTetrahedrons = 0;
+  for (size_t vertexIndex = 0; vertexIndex < indices.size(); vertexIndex += 3) {
+
+    glm::vec3 vertex1 = glm::vec3(0.0f);
+    glm::vec3 vertex2 = vertices[indices[vertexIndex] * 3];
+    glm::vec3 vertex3 = vertices[indices[vertexIndex + 1] * 3];
+    glm::vec3 vertex4 = vertices[indices[vertexIndex + 2] * 3];
+
+    glm::mat3 jacobian = glm::mat3(vertex2, vertex3, vertex4);
+    float det = glm::determinant(jacobian);
+
+    if (verbose) {
+      std::cout << "First Vertex: " << std::endl;
+      std::cout << vertex2.x << "\t" << vertex2.y << "\t" << vertex2.z
+                << std::endl;
+      std::cout << "Second Vertex: " << std::endl;
+      std::cout << vertex3.x << "\t" << vertex3.y << "\t" << vertex3.z
+                << std::endl;
+      std::cout << "Third Vertex: " << std::endl;
+      std::cout << vertex4.x << "\t" << vertex4.y << "\t" << vertex4.z
+                << std::endl;
+
+      std::cout << "Jacobian:" << std::endl;
+      std::cout << jacobian[0][0] << "\t" << jacobian[0][1] << "\t"
+                << jacobian[0][2] << "\t" << std::endl;
+
+      std::cout << jacobian[1][0] << "\t" << jacobian[1][1] << "\t"
+                << jacobian[1][2] << "\t" << std::endl;
+
+      std::cout << jacobian[2][0] << "\t" << jacobian[2][1] << "\t"
+                << jacobian[2][2] << "\t" << std::endl;
+    }
+    std::cout << "Determinant of the jacobian : " << det << std::endl;
+
+    float abcx = vertex1.x * vertex1.x + vertex1.x * vertex2.x +
+                 vertex2.x * vertex2.x + vertex1.x * vertex3.x +
+                 vertex2.x * vertex3.x + vertex3.x * vertex3.x +
+                 vertex1.x * vertex4.x + vertex2.x * vertex4.x +
+                 vertex3.x * vertex4.x + vertex4.x * vertex4.x;
+
+    float abcy = vertex1.y * vertex1.y + vertex1.y * vertex2.y +
+                 vertex2.y * vertex2.y + vertex1.y * vertex3.y +
+                 vertex2.y * vertex3.y + vertex3.y * vertex3.y +
+                 vertex1.y * vertex4.y + vertex2.y * vertex4.y +
+                 vertex3.y * vertex4.y + vertex4.y * vertex4.y;
+
+    float abcz = vertex1.z * vertex1.z + vertex1.z * vertex2.z +
+                 vertex2.z * vertex2.z + vertex1.z * vertex3.z +
+                 vertex2.z * vertex3.z + vertex3.z * vertex3.z +
+                 vertex1.z * vertex4.z + vertex2.z * vertex4.z +
+                 vertex3.z * vertex4.z + vertex4.z * vertex4.z;
+    float a = (det * (abcy + abcz) / 60.0f) / (det / 6.0);
+    float b = (det * (abcx + abcz) / 60.0f) / (det / 6.0);
+    float c = (det * (abcx + abcy) / 60.0f) / (det / 6.0);
+
+    float abcxp =
+        vertex2.y * vertex1.z + vertex3.y * vertex1.z + vertex4.y * vertex1.z +
+        vertex1.y * vertex2.z + vertex3.y * vertex2.z + vertex4.y * vertex2.z +
+        vertex1.y * vertex3.z + vertex2.y * vertex3.z + vertex4.y * vertex3.z +
+        vertex1.y * vertex4.z + vertex2.y * vertex4.z + vertex3.y * vertex4.z +
+        2.0f * vertex1.y * vertex1.z + 2.0f * vertex2.y * vertex2.z +
+        2.0f * vertex3.y * vertex3.z + 2.0f * vertex4.y * vertex4.z;
+
+    float abcyp =
+        vertex2.x * vertex1.z + vertex3.x * vertex1.z + vertex4.x * vertex1.z +
+        vertex1.x * vertex2.z + vertex3.x * vertex2.z + vertex4.x * vertex2.z +
+        vertex1.x * vertex3.z + vertex2.x * vertex3.z + vertex4.x * vertex3.z +
+        vertex1.x * vertex4.z + vertex2.x * vertex4.z + vertex3.x * vertex4.z +
+        2.0f * vertex1.x * vertex1.z + 2.0f * vertex2.x * vertex2.z +
+        2.0f * vertex3.x * vertex3.z + 2.0f * vertex4.x * vertex4.z;
+
+    float abczp =
+        vertex2.x * vertex1.y + vertex3.x * vertex1.y + vertex4.x * vertex1.y +
+        vertex1.x * vertex2.y + vertex3.x * vertex2.y + vertex4.x * vertex2.y +
+        vertex1.x * vertex3.y + vertex2.x * vertex3.y + vertex4.x * vertex3.y +
+        vertex1.x * vertex4.y + vertex2.x * vertex4.y + vertex3.x * vertex4.y +
+        2.0f * vertex1.x * vertex1.y + 2.0f * vertex2.x * vertex2.y +
+        2.0f * vertex3.x * vertex3.y + 2.0f * vertex4.x * vertex4.y;
+
+    float ap = (det * abcxp / 120.0f) / (det / 6.0);
+    float bp = (det * abcyp / 120.0f) / (det / 6.0);
+    float cp = (det * abczp / 120.0f) / (det / 6.0);
+
+    float tetrahedronInertiaTensorArray[] = {a,   -bp, -cp, -bp, b,
+                                             -ap, -cp, -ap, c};
+    glm::mat3 tetrahedronInertiaTensor =
+        glm::make_mat3(tetrahedronInertiaTensorArray);
+    if (verbose) {
+
+      std::cout << "Acumulated tetrahedron inertial tensor:" << std::endl;
+      std::cout << tetrahedronInertiaTensor[0][0] << "\t"
+                << tetrahedronInertiaTensor[0][1] << "\t"
+                << tetrahedronInertiaTensor[0][2] << "\t" << std::endl;
+
+      std::cout << tetrahedronInertiaTensor[1][0] << "\t"
+                << tetrahedronInertiaTensor[1][1] << "\t"
+                << tetrahedronInertiaTensor[1][2] << "\t" << std::endl;
+
+      std::cout << tetrahedronInertiaTensor[2][0] << "\t"
+                << tetrahedronInertiaTensor[2][1] << "\t"
+                << tetrahedronInertiaTensor[2][2] << "\t" << std::endl;
+    }
+    acumulatedInertialTensor += tetrahedronInertiaTensor;
+    numsOfTetrahedrons++;
+  }
+  std::cout << "Number of tetrahedrons: " << numsOfTetrahedrons << std::endl;
+  return acumulatedInertialTensor / static_cast<float>(numsOfTetrahedrons);
 }
--- utils.h
+++ utils.h
@@ -1,4 +1,42 @@
 #pragma once
-#include <vector>
+#include <array>
+#include <cstdint>
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <span>
 #include <string>
+#include <vector>
+#include "tiny_gltf.h"
+
 std::vector<char> readFile(const std::string &filename);
+
+constexpr bool isTowardsPlaneNormal(std::array<glm::vec3, 3> triangleplane,
+                                    glm::vec3);
+
+tinygltf::Model loadGltfFile(const std::string &path);
+
+std::vector<std::span<glm::vec3>>
+getVerticeData(const tinygltf::Model &model, const std::string &attributeName);
+
+class IndexDataSpan {
+public:
+
+  template<typename T>
+  std::span<T> getIndexSpan();
+
+private:
+  std::span<uint8_t> m_indexSpan;
+  uint8_t m_bytesPerInt;
+};
+template<typename T>
+std::vector<std::span<T>> getIndexData(const tinygltf::Model &model);
+
+template<typename T>
+constexpr glm::vec3 getCenterOfMass(const std::span<glm::vec3> &vertices,
+                                    const std::span<T> &indices,
+                                    const bool &verbose);
+template<typename T>
+constexpr glm::mat4 getInertiaTensor(const std::span<glm::vec3> &vertices,
+                                     const std::span<T> &indices,
+                                     const bool &verbose);
